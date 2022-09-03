1c877c6fde774d7259d3ea5ec6befcb519567a2f
ZoranDEV <zoranpavicevic098@gmail.com>
2022-09-03 16:14:09 +0200

    cleaning weather details

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -4,42 +4,48 @@ import WeatherContext from "../context/WeatherContext";
 const WeatherDetails = () => {
   // Context
   const {
-    searchedCityWeatherInfo: { currentWeather },
+    searchedCityWeatherInfo: {
+      currentWeather,
+      currentWeather: {
+        main: { feels_like, temp_max, temp_min, humidity, pressure },
+        wind: { speed },
+      },
+    },
   } = useContext(WeatherContext);
+
+  console.log(feels_like);
 
   return (
     <div className="w-full mb-4">
       <h3 className="text-xl text-neutral-100 mb-3">Weather details</h3>
-      {/* Feels like */}
-      <div className="w-full flex items-center justify-between text-md text-neutral-400 mb-1">
-        <h2>Feel like :</h2>
-        <h2>{Math.round(currentWeather.main.feels_like)} °C</h2>
-      </div>
-      {/* Max temp */}
-      <div className="w-full flex items-center justify-between text-md text-neutral-400  mb-1">
-        <h2>Max temp :</h2>
-        <h2>{Math.round(currentWeather.main.temp_max)} °C</h2>
-      </div>
-      {/* Min temp */}
-      <div className="w-full flex items-center justify-between text-md text-neutral-400  mb-1">
-        <h2>Min temp :</h2>
-        <h2>{Math.round(currentWeather.main.temp_min)} °C</h2>
-      </div>
-      {/* Humidity */}
-      <div className="w-full flex items-center justify-between text-md text-neutral-400  mb-1">
-        <h2>Humidity :</h2>
-        <h2>{currentWeather.main.humidity} %</h2>
-      </div>
-      {/* Pressure */}
-      <div className="w-full flex items-center justify-between text-md text-neutral-400  mb-1">
-        <h2>Pressure :</h2>
-        <h2>{currentWeather.main.pressure} hPa</h2>
-      </div>
-      {/* Wind speed */}
-      <div className="w-full flex items-center justify-between text-md text-neutral-400  mb-1">
-        <h2>Wind speed :</h2>
-        <h2>{Math.round(currentWeather.wind.speed * 3.6)} km/h</h2>
-      </div>
+      {[
+        "Feels like :",
+        "Max temp :",
+        "Min temp :",
+        "Humidity :",
+        "Pressure :",
+        "Wind speed :",
+      ].map((item, index) => (
+        <div
+          className="w-full flex items-center justify-between text-md text-neutral-400 mb-1"
+          key={index}
+        >
+          <h2>{item}</h2>
+          {item === "Feels like :" ? (
+            <h2>{Math.round(feels_like)} °C</h2>
+          ) : item === "Max temp :" ? (
+            <h2>{Math.round(temp_max)} °C</h2>
+          ) : item === "Min temp :" ? (
+            <h2>{Math.round(temp_min)} °C</h2>
+          ) : item === "Humidity :" ? (
+            <h2>{humidity} %</h2>
+          ) : item === "Pressure :" ? (
+            <h2>{pressure} hPa</h2>
+          ) : (
+            <h2>{Math.round(speed * 3.6)} km/h</h2>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
